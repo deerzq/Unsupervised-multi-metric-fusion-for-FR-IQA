afd4c794eb600aea6dcac636c60fa6420bfba7ab
deerzq <deerzq@gmail.com>
2014-07-11 12:56:00 -0700

    modify RRF.m

--- RRF.m
+++ RRF.m
@@ -1,4 +1,4 @@
-function rrf = RRF(scores, k, w)
+function rrf = RRF(scores, k)
 % rrf: rrf score for each object
 % t: rank of each object
 
@@ -10,19 +10,11 @@ if ~exist('k','var'),
     k = 60;
 end
 
-if ~exist('w','var'),
-    for i=1:K,
-        [~, r] = sort(scores(:,i), 'descend'); % r(k)-th object is ranked in the k-th place, score is the bigger the better
-		rrf(r) = rrf(r)+1./(k+(1:N)'); % rrf score: the bigger the better
-    end
-else
-    for i=1:K,
-        [~, r] = sort(scores(:,i), 'descend'); % r(k)-th object is ranked in the k-th place, score is the bigger the better
-        for j=1:N,
-            rrf(r(j)) = rrf(r(j))+1/(k+j)*w(i);     % rrf score: the bigger the better
-        end
-    end    
+for i=1:K,
+    [~, r] = sort(scores(:,i), 'descend'); % r(k)-th object is ranked in the k-th place, score is the bigger the better
+	rrf(r) = rrf(r)+1./(k+(1:N)'); % rrf score: the bigger the better
 end
+
 
 
 
